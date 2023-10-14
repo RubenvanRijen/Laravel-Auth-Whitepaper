5750b25a1ab89450a19962841b779797eb30939f
Ruben van Rijen <rubenvanrijen@gmail.com>
2023-10-14 20:46:50 +0200

    swagger doc bug fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,29 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="StickerWave-API",
+ *    description="Sticker wave Api",
+ *    version="1.0.0",
+ * ) 
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @OA\SecurityScheme(
+     *     type="http",
+     *     description="JWT Token",
+     *     name="Authorization",
+     *     in="header",
+     *     scheme="bearer",
+     *     bearerFormat="JWT",
+     *     securityScheme="jwt_token",
+     * )
+     */
+    public function documentation()
+    {
+    }
 }
